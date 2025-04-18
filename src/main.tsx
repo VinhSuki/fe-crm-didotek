f9765aa18793d80cbd60009c7db1d387e7962220
VinhSuki <vinhbestyasuo@gmail.com>
2025-04-18 10:06:34 +0700

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import store from "@/redux/store.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +13,6 @@ createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
   </Router>
